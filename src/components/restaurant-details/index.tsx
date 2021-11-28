41efe71f6457c071b08f1dbd82236e7456da37eb
Md Ashik <mostofabilla@gmail.com>
2021-11-28 14:26:02 +0600

    fate: show restaurant details from redux data  - on click restaurant details show from redux data.  - google map zoom change to 16  - Remove dummy restautrant info.

--- src/components/restaurant-details/index.tsx
+++ src/components/restaurant-details/index.tsx
@@ -4,53 +4,17 @@ import { Avatar, Typography, Space, Drawer } from 'antd';
 import './restaurant-details.scss';
 
 import GoogleMapView from '../google-map';
+import { useSelector } from 'react-redux';
+import { Restaurant } from '../../models/restaurant';
 
 const { Title } = Typography;
 
 function RestaurantDetails(props: any) {
-    const restaurant: any = {
-        fsq_id: 'object_id',
-        categories: [
-            {
-                id: '457',
-                name: 'Asian Restaurant',
-                icon: {
-                    prefix: 'https://ss3.4sqi.net/img/categories_v2/food/asian_',
-                    suffix: '.png',
-                },
-            },
-        ],
-        geocodes: {
-            main: {
-                latitude: 23.793330075666095,
-                longitude: 90.40322404501005,
-            },
-        },
-        location: {
-            address: 'House #15, Road #17, Block #C, Banani',
-            cross_street: 'Mir Showkat Avenue, Gulshan 2',
-            locality: 'Dhaka',
-            postcode: '1204',
-        },
-        name: 'Star Kabab & Restaurant',
-        popularity: 0.9024184476940382,
-        price: 2,
-        rating: 7.5,
-        tel: '01798-779900',
-    };
+    const mapZoomLevel: number = 16;
+    const restaurant: Restaurant = useSelector((state: any) => state.restaurant.selectedRestaurant);
 
     const { visible, onClose } = props;
-    const {
-        name,
-        categories,
-        geocodes: { main: geocodes },
-        location,
-        popularity,
-        price,
-        rating,
-        tel,
-    } = restaurant;
-    const { address, cross_street: crossStreet, locality, postcode } = location;
+    const { name, category, geocode, address, popularity, price, rating, iconUrl } = restaurant;
     return (
         <Drawer
             className={'restaurant-details-drawer'}
@@ -61,26 +25,17 @@ function RestaurantDetails(props: any) {
             visible={visible}
             style={{ padding: 0 }}
         >
-            <GoogleMapView lat={geocodes.latitude} lng={geocodes.longitude} zoom={18} />
+            <GoogleMapView lat={geocode.latitude} lng={geocode.longitude} zoom={mapZoomLevel} />
             <div className="restaurant-details">
                 <Title level={2}>{name}</Title>
-                <Title level={5}>
-                    {`${address}, ${crossStreet ? crossStreet + ',' : ''}
-                    ${locality ? locality + ',' : ''} ${postcode}`}
-                </Title>
-                {categories?.map((category: any, index: number) => (
-                    <Space size={10} className="categories" key={index}>
-                        <Avatar src={category.icon.prefix + '120' + category.icon.suffix} />
-                        <span>{category.name}</span>
-                    </Space>
-                ))}
+                <Title level={5}>{address}</Title>
+
+                <Space size={10} className="categories">
+                    <Avatar src={iconUrl} />
+                    <span>{category}</span>
+                </Space>
 
                 <ul>
-                    {!!tel && (
-                        <li>
-                            <b>Contact:</b> {tel}
-                        </li>
-                    )}
                     {!!popularity && (
                         <li>
                             <b>Popularity:</b> {popularity}
--- src/components/restaurant-item/index.tsx
+++ src/components/restaurant-item/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Avatar, Button, List } from 'antd';
 import './Restaurant-item.scss';
+import { useDispatch } from 'react-redux';
+import { RestaurantActions } from '../../data-store/actions/restaurant';
 
 export const RestaurantItem = (props: any) => {
+    const dispatch = useDispatch();
     let { name, iconUrl, address } = props?.restaurant;
+
+    const openRestaurantDetails = () => {
+        props.openRestaurantDetails();
+        dispatch({ type: RestaurantActions.SELECTED_RESTAURANT, payload: props?.restaurant });
+    };
+
     return (
         <List.Item.Meta
             className="restaurant-item"
@@ -12,7 +21,7 @@ export const RestaurantItem = (props: any) => {
             description={
                 <>
                     <p style={{ marginBottom: '5px' }}>{address}</p>
-                    <Button type="primary" size={'small'} onClick={props.openRestaurantDetails}>
+                    <Button type="primary" size={'small'} onClick={openRestaurantDetails}>
                         Details
                     </Button>
                 </>
