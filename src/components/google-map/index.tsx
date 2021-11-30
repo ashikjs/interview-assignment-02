8754f60c77ed65bd0903bd72c1444aa9756f4fbe
Md Ashik <mostofabilla@gmail.com>
2021-11-30 12:30:02 +0600

    fix: solved google map load error

--- src/components/google-map/index.tsx
+++ src/components/google-map/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 
 function GoogleMapView(props: any) {
     const { lat, lng, zoom, height = '260px', width = '100%' } = props;
     const apiKey = process.env.REACT_APP_MAP_API_KEY;
-    return (
-        <LoadScript googleMapsApiKey={`${apiKey}`}>
+
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: apiKey ? apiKey : '',
+    });
+    const renderMap = () => {
+        return (
             <GoogleMap mapContainerStyle={{ height, width }} center={{ lat, lng }} zoom={zoom}>
                 <Marker position={{ lat, lng }} />
             </GoogleMap>
-        </LoadScript>
-    );
+        );
+    };
+
+    if (loadError) {
+        return <div>Map cannot be loaded right now, sorry.</div>;
+    }
+
+    return isLoaded ? renderMap() : <p>Loading...</p>;
 }
 
 export default React.memo(GoogleMapView);
